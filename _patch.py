e94fcc5751e2320279af8906a9dcf45d8a6ae091
hovtsamsung5677 <hovtsamsung5677@gmail.com>
2026-07-07 10:11:09 +0300

    добавление 4 шагов123

---
+++ _patch.py
@@ -0,0 +1,4 @@
+import re
+f=open(chr(115)+chr(101)+chr(114)+chr(118)+chr(101)+chr(114)+chr(47)+chr(97)+chr(112)+chr(112)+chr(46)+chr(112)+chr(121),chr(114)+chr(101)+chr(110)+chr(99)+chr(111)+chr(100)+chr(105)+chr(110)+chr(103)+chr(61)+chr(34)+chr(117)+chr(116)+chr(102)+chr(45)+chr(56)+chr(34)); content=f.read(); f.close()
+old=content[content.find(chr(98)+chr(101)+chr(115)+chr(116)+chr(95)+chr(105)+chr(100)+chr(120)+chr(32)+chr(61)+chr(32)+chr(110)+chr(112)+chr(46)+chr(97)+chr(114)+chr(103)+chr(109)+chr(97)+chr(120)+chr(40)+chr(115)+chr(99)+chr(111)+chr(114)+chr(101)+chr(115)+chr(41)):][:350]
+print(old)
